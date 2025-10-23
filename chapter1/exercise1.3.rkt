e93695ff368b476a66543a85eed8f360fcd9e3f5
michelef <michele.franzoni@muscope.com>
2025-10-23 08:48:37 +0200

    feat(exercise1.3): greatest number of a list works, figuring out the greatest pair of a list (WIP)

--- chapter1/exercise1.3.rkt
+++ chapter1/exercise1.3.rkt
@@ -5,13 +5,36 @@
 ;I also expect the helper function to be "abstractable" from the cardinality of the tuplet
 ;but that's the next step 
 
+
+;HELPER FUNCTION
 (define (max-of-two x y)
   (if (> x y) x y)
 )
 
+;MAIN FUNCTION
 (define (greatest-of-many numbers)
   (cond
     ((null? numbers) '())
     ((null? (cdr numbers)) (car numbers))
     (else (max-of-two (greatest-of-many (cdr numbers))
                       (car numbers)))))
+
+
+;HELPER FUNCTIONS
+(define (update-max-pair pair candidate)
+  (cond
+    ((> candidate (car pair)) (cons candidate (cdr pair) )) 
+    ((> candidate (cadr pair)) (cons (car pair) candidate ))
+    (else pair)
+  )
+)
+
+;MAIN FUNCTION
+(define (greatest-pair numbers)
+  (cond 
+    ((null? numbers) '())
+    ((null? (cdr numbers)) '())
+    ((null? (cddr numbers)) numbers)
+    ((update-max-pair (greatest-pair (cdr numbers)) (car numbers)))
+  )
+)
