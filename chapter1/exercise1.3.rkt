fa643e9c81061fc84f8c1438bc58f563b0f664f7
michelef <michele.franzoni@muscope.com>
2025-10-24 07:34:59 +0200

    feat(chapter1): implemented working solution for the 1.3 exercise
    the greates-pair procedure is a bit of an overkill

--- chapter1/exercise1.3.rkt
+++ chapter1/exercise1.3.rkt
@@ -1,27 +1,22 @@
 #lang scheme
-;max-of-two behave like a "helper" function that is called in the recursive step, I expect a similar
-;(but slightly more complex) helper for the greatest pair
-;then for the greatest triplet and so on
-;I also expect the helper function to be "abstractable" from the cardinality of the tuplet
-;but that's the next step 
-
+;#TODO: Implement a generalized function that can find the n greatest numbers in a list.
 ;###########################################GREATEST SINGLET###########################################
-;HELPER FUNCTION
-(define (max-of-two x y)
-  (if (> x y) x y)
-)
+; ;HELPER FUNCTION
+; (define (max-of-two x y)
+;   (if (> x y) x y)
+; )
 
-;MAIN FUNCTION
-(define (greatest-of-many numbers)
-  (cond
-    ((null? numbers) '())
-    ((null? (cdr numbers)) (car numbers))
-    (else (max-of-two (greatest-of-many (cdr numbers))
-                      (car numbers)))))
+; ;MAIN FUNCTION
+; (define (greatest-of-many numbers)
+;   (cond
+;     ((null? numbers) '())
+;     ((null? (cdr numbers)) (car numbers))
+;     (else (max-of-two (greatest-of-many (cdr numbers))
+;                       (car numbers)))))
 
-;###########################################GREATEST PAIR###########################################
+; ;###########################################GREATEST PAIR###########################################
 
-;HELPER FUNCTIONS
+;this helper function updates the pair if the candidate is greater than either of the two values in the pair
 (define (update-max-pair pair candidate)
   (cond
     ((> candidate (car pair)) (cons candidate (cdr pair) )) 
@@ -30,6 +25,7 @@
   )
 )
 
+;this helper function orders the pair in ascending order
 (define (order-pair pair)
   (if (> (car pair) (cadr pair))
       (list (cadr pair) (car pair)) 
@@ -37,12 +33,27 @@
   )
 )
 
-;MAIN FUNCTION
+;the main function recursively finds the greatest pair in the list
 (define (greatest-pair numbers)
   (cond 
     ((null? numbers) '())
     ((null? (cdr numbers)) '())
     ((null? (cddr numbers)) numbers)
     (else (order-pair(update-max-pair (order-pair (greatest-pair (cdr numbers))) (car numbers))))
+  )
+)
+
+;define the function that gives the square of a number
+(define (square x)
+  (* x x)
+)
+;define the final function that returns the sum of the squares of the greatest pairs in a triplet 
+(define (sum-square-of-greatests triplet)
+  ( + (square
+        (car 
+          (greatest-pair triplet)))
+      (square
+        (cadr
+          (greatest-pair triplet)))
   )
 )
