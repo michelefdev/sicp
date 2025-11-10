1a628933643ebcef3ea7a930763fc220e68dfa11
michelef <michele.franzoni@muscope.com>
2025-11-10 21:56:17 +0100

    feat(chaper1): added exercise 11, recursive implementation working, iterative WIP

---
+++ chapter1/exercises/exercise1.11.rkt
@@ -0,0 +1,26 @@
+; f(n)= n if n < 3
+; f(n)= f(n-1) + 2*f(n-2) + 3*f(n-3) if n >= 3
+
+; Write a procedure that computes f by means of a recursive
+; process. Write a procedure that computes f by means of an
+; iterative process.
+
+(define (f-recur n)
+    (if (< n 3) n 
+        (+ 
+            (f-recur (- n 1))
+            (* 2 (f-recur (- n 2)))
+            (* 3 (f-recur (- n 3)))
+        )
+    )
+)
+
+(define (f-iter n)
+    (define (f first second third counter)
+        (if (= counter 0)
+            third
+            (f third (* 2 second) (* 3 first) (- counter 1))
+        ) 
+    )    
+    (f 0 1 2 counter)
+)
