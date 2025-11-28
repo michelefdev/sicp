37acbb6de8c88b1f64c1a562f8459350e97d8bb0
michelefdev <michele.franzoni.dev@gmail.com>
2025-11-28 19:58:15 +0100

    fix(chapter1): renamed the exercise as the correct one
    exercise 17 requires a recursive process analogous to fast exp, the iterative approach is for exercise 1.18

--- chapter1/exercises/exercise1.18.rkt
+++ chapter1/exercises/exercise1.18.rkt
@@ -1,6 +1,5 @@
-; suppose we include, together with addition, operations double, which doubles an integer, and halve, which
-; divides an (even) integer by 2. Using these, design a multiplication procedure analogous to fast-expt 
-; that uses a logarithmic number of steps.
+; devise a procedure that generates an iterative process for multiplying two integers in terms of
+; adding, doubling, and halving and uses a logarithmic number of steps
 
 (define (fast-mult a b)
 
