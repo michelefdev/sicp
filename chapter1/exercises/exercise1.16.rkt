0b3abb76957b09dce17ed6d2ec3d0539c8f7bc38
michelefdev <michele.franzoni.dev@gmail.com>
2025-11-28 07:53:18 +0100

    feat(chapter1): solved exercise 16

--- chapter1/exercises/exercise1.16.rkt
+++ chapter1/exercises/exercise1.16.rkt
@@ -1,2 +1,15 @@
 ; Design a procedure that evolves an iterative exponentiation process that 
 ; uses successive squaring and uses a logarithmic number of steps
+
+(define (fast-exp b n)
+    (define (square x)
+        (* x x)
+    )
+    (define (fast-exp-iter a b n
+        )
+        (cond ((= n 0) a)
+              ((even? n) (fast-exp-iter a (square b) (/ n 2)))
+              (else (fast-exp-iter (* a b) b (- n 1))))
+    )
+    (fast-exp-iter 1 b n)
+)
